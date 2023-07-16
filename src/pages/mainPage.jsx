4378a940e283ef4a945d546a904b2c0bab2bdd1d
woong <inwoong100@gmail.com>
2023-07-17 00:05:10 +0900

    feat: add reatRouter

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,12 @@
+import Footer from '../components/footer';
+import Header from '../components/header';
+
+export default function MainPage() {
+  return (
+    <>
+      <Header />
+      <div>contents</div>
+      <Footer />
+    </>
+  );
+}
