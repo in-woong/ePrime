3fa45bd1b2c18cf6a395994a3f3dcea6203700e5
woong <inwoong100@gmail.com>
2023-07-24 23:35:02 +0900

    feat: set form

--- src/components/popupMenus.jsx
+++ src/components/popupMenus.jsx
@@ -6,15 +6,21 @@ export default function PopupMenus() {
       <ul className='grid grid-rows-5 w-[520px] h-full my-0 px-auto gap-1'>
         <li className='flex justify-between items-center'>
           ESG 경영 전략 수립시 양질의 원료가 필요하신 분!{' '}
-          <button className='text-xl font-bold hover:text-gray-500'>
-            <Link to='form/esg'>Click</Link>
-          </button>
+          <Link
+            to='/form_esg'
+            className='text-xl font-bold hover:text-gray-500'
+          >
+            Click
+          </Link>
         </li>
         <li className='flex justify-between items-center'>
           폐자원(Pellet, Flake 포함)을 사고 파는데 도움이 필요하신 분!
-          <button className='text-xl font-bold hover:text-gray-500'>
+          <Link
+            to='form_product'
+            className='text-xl font-bold hover:text-gray-500'
+          >
             Click
-          </button>
+          </Link>
         </li>
         <li className='flex justify-between items-center'>
           24 스마트생태공장구축 지원사업 신청하시고자 하는분!
